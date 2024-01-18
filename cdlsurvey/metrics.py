27602c139646f59d951c76d2d41c43604746ff91
Ryan <ryan@tesorio.com>
2024-01-18 14:14:10 -0800

    updating data and notebooks

--- cdlsurvey/metrics.py
+++ cdlsurvey/metrics.py
@@ -9,10 +9,10 @@ PROTECTED_COLUMNS = ['gender_Female', 'gender_Male', 'race_White', 'race_Black']
 
 def error_rate(predictions, labels):
     """
-    Computes the error rate
+    Function that computes the error rate
     """
     signed_labels = (labels > 0).astype(np.float32) - (labels <= 0).astype(np.float32)
-    numerator = (np.multiply(signed_labels, predictions) <= 0).sum()
+    numerator = (np.multiply(signed_labels.values, predictions.values) <= 0).sum()
     denominator = predictions.shape[0]
     return float(numerator) / float(denominator)
 
@@ -28,7 +28,7 @@ def tpr(df: pd.DataFrame) -> float:
 
 def _get_error_rate_and_constraints(df, tpr_max_diff):
     """
-    Computes the error and fairness violations.
+    Function that computes the error and fairness violations.
     """
     error_rate_local = error_rate(df[['predictions']], df[[LABEL_COLUMN]])
     overall_tpr = tpr(df)
@@ -36,3 +36,13 @@ def _get_error_rate_and_constraints(df, tpr_max_diff):
         (overall_tpr - tpr_max_diff) - tpr(df[df[protected_attribute] > 0.5])
         for protected_attribute in PROTECTED_COLUMNS
     ]
+
+
+def get_exp_error_rate_constraints(cand_dist, error_rates_vector, constraints_matrix):
+    """
+    Function that computes the expected error and
+    fairness violations on a randomized solution.
+    """
+    expected_error_rate = np.dot(cand_dist, error_rates_vector)
+    expected_constraints = np.matmul(cand_dist, constraints_matrix)
+    return expected_error_rate, expected_constraints
